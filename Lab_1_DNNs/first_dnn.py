ce6f4b02cb35b11f9a7f3b8813f00bd98b31c55a
tristanfrench <tw15220@my.bristol.ac.uk>
2018-10-14 21:01:37 +0100

    doing NN

--- Lab_1_DNNs/first_dnn.py
+++ Lab_1_DNNs/first_dnn.py
@@ -37,16 +37,42 @@ groundTruth = np.argmax(test_y.values,1)#array with index of +1 label for each r
 
 x = tf.placeholder(tf.float32, shape=[None, n_x])
 y = tf.placeholder(tf.float32, shape=[None, n_y])
-
 w = tf.Variable(tf.zeros([n_x,n_y]))
 b = tf.Variable(tf.zeros([n_y]))
 
+n_1 = 10 #number of nodes of hidden layer 1
+n_2 = 20
+n_3 = 10
+
+weights = {
+        'w_1': tf.Variable(tf.truncated_normal([n_x, n_1], stddev=0.1)),
+        'w_2': tf.Variable(tf.truncated_normal([n_1, n_2], stddev=0.1)),
+        'w_3': tf.Variable(tf.truncated_normal([n_2, n_3], stddev=0.1)),
+        'w_y': tf.Variable(tf.truncated_normal([n_3, n_y], stddev=0.1))
+}
+
+bias = {
+        'b_1': tf.Variable(tf.constant(0.1, shape=[n_1])),
+        'b_2': tf.Variable(tf.constant(0.1, shape=[n_2])),
+        'b_3': tf.Variable(tf.constant(0.1, shape=[n_3])),
+        'b_y': tf.Variable(tf.constant(0.1, shape=[n_y]))
+}
+
+
+h_1 = tf.nn.relu(tf.matmul(x, weights['w_1']) + bias['b_1'])
+h_2 = tf.nn.relu(tf.matmul(h_1, weights['w_2']) + bias['b_2'])
+h_3 = tf.nn.relu(tf.matmul(h_2, weights['w_3']) + bias['b_3'])
+predictions_fcn = tf.nn.relu(tf.matmul(h_3, weights['w_y']) + bias['b_y'])
+
+
+cost_fcn = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=predictions_fcn, scope="Cost_Function")
+'''
 prediction = tf.nn.softmax(tf.matmul(x,w) + b)
 
 cost = tf.reduce_mean(-tf.reduce_sum(y * tf.log(prediction), axis=1))
 
 optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
-sess.run(tf.global_variables_initializer())
+sess.run(tf.global_variables_initializer()) 
 
 for epoch in range(10000):
     sess.run([optimizer], feed_dict={x: train_x, y: train_y})
@@ -54,7 +80,7 @@ for epoch in range(10000):
     myPrediction = np.argmax(myPrediction,1)
     if epoch % 1000 == 0:
         print('Accuracy at epoch:%d =' %epoch,sum(np.equal(myPrediction,groundTruth))/len(groundTruth))
-
+'''
     
     
     
